f5bb611940aa5f5d30b97e11209b72dd6bcef804
jefersonlimacj <jeferson.lima.cj@gmail.com>
2025-08-30 21:11:03 -0300

    rt

--- src/telas/subtelas/rotas/rotas.tsx
+++ src/telas/subtelas/rotas/rotas.tsx
@@ -1,19 +1,31 @@
 import BaseTelas from "../../../componentes/baseTelas";
 import EditPerfil from "../../../componentes/editPerfil";
+import styled from "styled-components";
 
 function Rotas() {
-  return  BaseTelas({
+  return BaseTelas({
     conteudo: (
       <>
         <EditPerfil />
         <ConteudoRotas />
       </>
     ),
-  });;
+  });
 }
 
 export default Rotas;
 
+const SelectEmpresa = styled.div`
+  width: 200px;
+  height: 50px;
+  background-color: red;
+  border-radius: 22px
+`;
+
 function ConteudoRotas() {
-  return <h1>Rotas</h1>;
+  return (
+    <div style={{ width: "100%", padding: 30 }}>
+      <SelectEmpresa />
+    </div>
+  );
 }
