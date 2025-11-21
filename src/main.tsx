6c59990098e560c70b259e1b1300974353730f26
jefersonlimacj <jeferson.lima.cj@gmail.com>
2025-11-21 11:29:29 -0300

    feat: Apontando para API Render

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { TemaProvider } from './hooks/temaContext.tsx'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { TemaProvider } from "./hooks/temaContext.tsx";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   // uri: 'https://neofrotaservico.onrender.com/graphql',
-  uri: 'http://localhost:4000/graphql',
+  // uri: 'http://localhost:4000/graphql',
   // uri: 'https://api.neofrota.com.br/graphql',
-  cache: new InMemoryCache()
-})
+  uri: "https://neofrotaapitemp-1.onrender.com/graphql",
+  cache: new InMemoryCache(),
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={client}>
-    <TemaProvider>
-      <App />
-    </TemaProvider>
+      <TemaProvider>
+        <App />
+      </TemaProvider>
     </ApolloProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
