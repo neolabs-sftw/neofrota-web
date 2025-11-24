c673dafe8009873c73c1198b70d295042537e755
jefersonlimacj <jeferson.lima.cj@gmail.com>
2025-11-24 09:36:20 -0300

    feat: troca da uri da API

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from "@apollo/c
 
 // Forma nova e oficial (sem warning)
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
+  // uri: 'http://localhost:4000/graphql',
   // uri: 'https://api.neofrota.com.br/graphql',
-  // uri: 'https://neofrotaapitemp-1.onrender.com/graphql',
+  uri: 'https://neofrotaapitemp-1.onrender.com/graphql',
   credentials: "include", // se você usa cookies/auth
 });
 
